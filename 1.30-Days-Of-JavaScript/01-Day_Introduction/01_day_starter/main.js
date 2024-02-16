cdaf2a2967651d4b0528521d4bbd3bbc02ad3398
LAHILOTE <mudher.d@gmail.com>
2024-02-16 21:31:43 +0800

    Day-1-Done

---
+++ 1.30-Days-Of-JavaScript/01-Day_Introduction/01_day_starter/main.js
@@ -0,0 +1,5 @@
+// nilai variabel dapat diakses dari file variable.js lainnya
+
+console.log(firstName, lastName, country, city, age, isMarried)
+console.log(gravity, boilingPoint, PI)
+console.log(name, job, live)
---
+++ 1.30-Days-Of-JavaScript/01-Day_Introduction/01_day_starter/variable.js
@@ -0,0 +1,24 @@
+// Mendeklarasikan variabel berbeda dari tipe data berbeda
+
+let firstName = 'Machmud' // nama depan seseorang
+let lastName = 'Domili' // nama belakang seseorang
+let country = 'Indonesia' // negara
+let city = 'Jakarta' // ibu Kota
+let age = 100 // Usia dalam bentuk tahun
+let isMarried = true
+
+
+
+
+// Mendeklarasikan variabel dengan nilai angka
+
+const gravity = 9.81 // gravitasi bumi dalam m/s2
+const boilingPoint = 100 // titik didih air, suhu dalam oC
+const PI = 3.14 // konstanta geometri
+
+
+
+// Variabel juga dapat dideklarasikan dalam satu baris dengan dipisahkan koma
+
+let name = 'Machmud', //nama seseorang
+    job = 'IT', live = 'Gorontalo'
